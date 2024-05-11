9f658f060e5b947e42b6aa0cce19bf0ac2fde7a6
julyfun on yuanzhi laptop-server <julyfun.collect@outlook.com>
2024-05-11 15:42:28 +0800

    tab

--- filter.fish
+++ filter.fish
@@ -1,6 +1,6 @@
 set types JointControllerState JointTrajectoryControllerState JointState
 for type in $types
-for line in (cat topic_type.txt)
+    for line in (cat topic_type.txt)
         if string match -rq $type'$' "$line"
             set cut_string (string match -r '.*(?=:)' "$line")
             echo \(\"$cut_string\", $type,\)
